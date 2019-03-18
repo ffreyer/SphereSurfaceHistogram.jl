4a20d74f45a08bd6bded45646bbc9be4f12737c1
F Freyer <frederic481994@hotmail.de>
2019-03-18 09:51:21 +0100

    Files generated by PkgTemplates

---
+++ src/SphereSurfaceHistogram.jl
@@ -0,0 +1,5 @@
+module SphereSurfaceHistogram
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SphereSurfaceHistogram
+using Test
+
+@testset "SphereSurfaceHistogram.jl" begin
+    # Write your own tests here.
+end
