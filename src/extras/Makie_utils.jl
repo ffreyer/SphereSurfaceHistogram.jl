69d49466ef47263598f172a8df0304d6e7081e3e
F Freyer <frederic481994@hotmail.de>
2019-04-13 10:38:42 +0200

    add to_alpha_hue

--- src/extras/Makie_utils.jl
+++ src/extras/Makie_utils.jl
@@ -40,6 +40,14 @@ function to_hue2(B::SSHBinner, s=1.0, v=1.0, a=1.0)
     end
 end
 
+function to_alpha_hue(B::SSHBinner, s=1.0, v=1.0)
+    # hue is cyclic, so the maximum should be < 1.0 (360)
+    bins = 0.8 * (B.bins .- minimum(B.bins)) ./ (maximum(B.bins) - minimum(B.bins))
+    map(bins) do k
+        HSVA(360k, s, v, min(1.0, 2k))
+    end
+end
+
 
 """
     to_alpha(B::SSHBinner[, r=0.2, g=0.4, b=0.8])
