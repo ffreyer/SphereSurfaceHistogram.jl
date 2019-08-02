b3629e6ceb93c331da0367d3f18d94cb15715380
Frederic Freyer <ffreyer@l45.thp.uni-koeln.de>
2019-08-02 16:44:01 +0200

    remove unused binary find

--- src/utils.jl
+++ src/utils.jl
@@ -1,18 +1,1 @@
-function binary_find(x, xs, start=1, stop=length(xs))
-    width = stop - start + 1
-    if width < 10
-        theta_index = 0
-        for i in start:stop-1
-            if x >= xs[i+1]
-                return i
-            end
-        end
-    else
-        middle = start + div(width, 2)
-        if x <= xs[middle]
-            return binary_find(x, xs, middle, stop)
-        else
-            return binary_find(x, xs, start, middle)
-        end
-    end
 end
